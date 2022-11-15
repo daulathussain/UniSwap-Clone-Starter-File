c4dc1af8697d58f33729b6c3b7facf8f43ff94b4
daulathussain <daulathussain92@gmail.com>
2022-11-15 10:00:27 +0530

    first commit

---
+++ assets/index.js
@@ -0,0 +1,21 @@
+import uniswap from "./uniswap.png";
+import search from "./Search.png";
+import ether from "./ether.png";
+import arrow from "./arrow.png";
+import arrowup from "./arrow up.png";
+import close from "./cross.png";
+import etherlogo from "./etherlogo.png";
+import homeicon from "./homeicon.png";
+import lock from "./lock.png";
+
+export default {
+  uniswap,
+  search,
+  ether,
+  arrow,
+  arrowup,
+  close,
+  etherlogo,
+  homeicon,
+  lock,
+};
